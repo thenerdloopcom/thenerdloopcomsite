7c82515e6aa6918c64f311b22464ed4e969ef5ce
Sidhant Mishra <sidhant.mishra2022@vitstudent.ac.in>
2026-09-08 00:20:09 +0530

    fix logo bg and size, remove slash lines

--- components/waitlist/decor.tsx
+++ components/waitlist/decor.tsx
@@ -13,14 +13,6 @@ export function Decor() {
           WebkitMaskImage: "radial-gradient(120% 90% at 50% 0%, black 30%, transparent 78%)",
         }}
       />
-
-      {/* Speed-line slashes */}
-      <div className="absolute -left-10 top-16 h-2 w-72 -rotate-[24deg] bg-tnl-black/80" />
-      <div className="absolute -left-6 top-24 h-1 w-40 -rotate-[24deg] bg-tnl-red/80" />
-      <div className="absolute right-[-40px] top-10 h-2 w-80 rotate-[18deg] bg-tnl-black/80" />
-      <div className="absolute right-4 top-20 h-1 w-44 rotate-[18deg] bg-tnl-blue/80" />
-      <div className="absolute bottom-24 left-[-30px] h-1.5 w-56 rotate-[12deg] bg-tnl-black/70" />
-      <div className="absolute bottom-16 right-[-30px] h-1.5 w-60 -rotate-[14deg] bg-tnl-black/70" />
     </div>
   )
 }
